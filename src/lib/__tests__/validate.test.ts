9165fa3e1f6ccfa30c51f02e17494de4381153f4
Anatoliy Oblaukhov <mail@anmiles.net>
2026-04-17 15:02:17 +0300

    Improve detailed messages for union errors

--- src/lib/__tests__/validate.test.ts
+++ src/lib/__tests__/validate.test.ts
@@ -2,19 +2,15 @@ import z from 'zod';
 
 import { validate } from '../validate';
 
-const schema = z.object({
-	id  : z.number(),
-	text: z.string(),
-	flag: z.boolean().optional(),
-});
-
 describe('src/lib/validate', () => {
 	describe('validate', () => {
 		it('should return data if validation passed', () => {
+			const schema = z.object({
+				key: z.string(),
+			});
+
 			const data = {
-				id  : 1,
-				text: 'value',
-				flag: true,
+				key: 'value',
 			};
 
 			const result = validate(data, schema);
@@ -22,14 +18,200 @@ describe('src/lib/validate', () => {
 			expect(result).toEqual(data);
 		});
 
-		it('should throw an error if validation failed', () => {
+		it('should handle invalid_type errors', () => {
+			const schema = z.object({
+				id: z.number(),
+				a : z.object({
+					text: z.string(),
+					b   : z.object({
+						text: z.string(),
+					}),
+					c: z.object({
+						text: z.string(),
+					}),
+					d: z.string().array(),
+					e: z.string().array(),
+				}),
+			});
+
 			const data = {
-				id: 'wrong',
+				id: 'a',
+				a : {
+					text: 1,
+					c   : {
+						text: 1,
+					},
+					d: 1,
+					e: [ 'a', 1 ],
+				},
 			};
 
 			expect(() => validate(data, schema)).toThrow(`Validation failed:
-	id (Invalid input: expected number, received string)
-	text (Invalid input: expected string, received undefined)`);
+	a.b (Invalid input: expected object, received undefined)
+	a.c.text (Invalid input: expected string, received number)
+	a.d (Invalid input: expected array, received number)
+	a.e.1 (Invalid input: expected string, received number)
+	a.text (Invalid input: expected string, received number)
+	id (Invalid input: expected number, received string)`);
+		});
+
+		it('should handle invalid_value errors', () => {
+			const schema = z.object({
+				key: z.enum([ 'value1', 'value2' ]),
+			});
+
+			const data = {
+				key: 'value3',
+			};
+
+			expect(() => validate(data, schema)).toThrow(`Validation failed:
+	key (Invalid option: expected one of "value1"|"value2")`,
+			);
+		});
+
+		it('should handle invalid_format errors', () => {
+			const schema = z.object({
+				email: z.email(),
+			});
+
+			const data = {
+				email: 'a',
+			};
+
+			expect(() => validate(data, schema)).toThrow(`Validation failed:
+	email (Invalid email address)`,
+			);
+		});
+
+		it('should handle invalid_union errors', () => {
+			const data = {
+				id: 'value',
+				a : {
+					textOrId1: true,
+				},
+			};
+
+			const schema = z.object({
+				id: z.number(),
+			}).or(z.object({
+				a: z.object({
+					textOrId1: z.string().or(z.number()),
+					textOrId2: z.union([ z.string(), z.number() ]),
+				}),
+			}));
+
+			expect(() => validate(data, schema)).toThrow(`Validation failed:
+	a.textOrId1 (Invalid input: expected string, received boolean)
+	a.textOrId1 (Invalid input: expected number, received boolean)
+	a.textOrId2 (Invalid input: expected string, received undefined)
+	a.textOrId2 (Invalid input: expected number, received undefined)
+	id (Invalid input: expected number, received string)`);
+		});
+
+		it('should handle invalid_key errors', () => {
+			const schema = z.object({
+				map1: z.map(z.number(), z.number()),
+			});
+
+			const data = {
+				map1: new Map([ [ true, 1 ] ]),
+			};
+
+			expect(() => validate(data, schema)).toThrow(`Validation failed:
+	map1 (Invalid key in map)`,
+			);
+		});
+
+		it('should handle invalid_element errors', () => {
+			const schema = z.object({
+				map1: z.map(z.number(), z.number()),
+			});
+
+			const data = {
+				map1: new Map([ [ true, true ] ]),
+			};
+
+			expect(() => validate(data, schema)).toThrow(`Validation failed:
+	map1 (Invalid key in map)
+	map1 (Invalid value in map)`,
+			);
+		});
+
+		it('should handle not_multiple_of errors', () => {
+			const schema = z.object({
+				price: z.number().multipleOf(1000),
+			});
+
+			const data = {
+				price: 999,
+			};
+
+			expect(() => validate(data, schema)).toThrow(`Validation failed:
+	price (Invalid number: must be a multiple of 1000)`);
+		});
+
+		it('should handle too_big errors', () => {
+			const schema = z.object({
+				id: z.number().min(10).max(19),
+			});
+
+			const data = {
+				id: 20,
+			};
+
+			expect(() => validate(data, schema)).toThrow(`Validation failed:
+	id (Too big: expected number to be <=19)`);
+		});
+
+		it('should handle too_small errors', () => {
+			const schema = z.object({
+				id: z.number().min(10).max(19),
+			});
+
+			const data = {
+				id: 5,
+			};
+
+			expect(() => validate(data, schema)).toThrow(`Validation failed:
+	id (Too small: expected number to be >=10)`);
+		});
+
+		it('should handle unrecognized_keys errors', () => {
+			const data = {
+				id      : 1,
+				unknown1: true,
+				a       : {
+					text    : 'value',
+					unknown2: false,
+				},
+			};
+
+			const schema = z.object({
+				id: z.number(),
+				a : z.object({
+					text: z.string(),
+				}).strict(),
+			}).strict();
+
+			expect(() => validate(data, schema)).toThrow(`Validation failed:
+	[root] (Unrecognized key: "unknown1")
+	a (Unrecognized key: "unknown2")`);
+		});
+
+		it('should handle custom errors', () => {
+			const schema = z.object({
+				text: z.string().refine((v) => v.startsWith('a'), {
+					message: 'Must start with a',
+				}),
+			});
+
+			const data = {
+				text: 'b',
+			};
+
+			expect(() => validate(data, schema)).toThrow(`Validation failed:
+	text (Must start with a)`,
+			);
 		});
 	});
 });
--- src/lib/validate.ts
+++ src/lib/validate.ts
@@ -1,15 +1,77 @@
+import type { z } from 'zod';
+
 import type { Schema } from '../types';
+
+function extractNestedIssues(issue: z.ZodIssue, parentPath: string[] = []): z.ZodIssue[] {
+	const path         = [ ...parentPath, ...issue.path.map(String) ];
+	const defaultValue = [ { ...issue, path } ];
+
+	switch (issue.code) {
+		case 'invalid_type':
+			return defaultValue;
+		case 'invalid_union':
+			return issue.errors.flat().map((error) => extractNestedIssues(error, path)).flat();
+		case 'invalid_format':
+			return defaultValue;
+		case 'invalid_value':
+			return defaultValue;
+		case 'invalid_key':
+			return defaultValue;
+		case 'invalid_element':
+			return defaultValue;
+		case 'not_multiple_of':
+			return defaultValue;
+		case 'too_big':
+			return defaultValue;
+		case 'too_small':
+			return defaultValue;
+		case 'unrecognized_keys':
+			return defaultValue;
+		case 'custom':
+			return defaultValue;
+	}
+}
+
+function sortIssues({ path: path1 }: z.ZodIssue, { path: path2 }: z.ZodIssue): number {
+	for (let i = 0; i < Math.max(path1.length, path2.length); i++) {
+		if (!path1[i]) { return -1; }
+
+		const diff = String(path1[i]).localeCompare(String(path2[i]));
+
+		if (diff !== 0) {
+			return diff;
+		}
+	}
+
+	return 0;
+}
+
+function formatIssue(issue: z.ZodIssue): string {
+	const fullPath = issue.path.length === 0
+		? '[root]'
+		: issue.path.join('.');
+
+	return `\t${fullPath} (${issue.message})`;
+}
+
+function formatError(error: z.ZodError): string[] {
+	return [
+		'Validation failed:',
+		...error.issues
+			.map((issue) => extractNestedIssues(issue))
+			.flat()
+			.sort(sortIssues)
+			.map(formatIssue),
+	];
+}
 
 export function validate<T>(data: unknown, schema: Schema<T>): T {
 	const parsed = schema.safeParse(data);
 
 	if (!parsed.success) {
-		const errorLines = [
-			'Validation failed:',
-			...parsed.error.issues.map((i) => `${i.path.join('.')} (${i.message})`),
-		];
+		const errorLines = formatError(parsed.error);
 
-		throw new Error(errorLines.join('\n\t'));
+		throw new Error(errorLines.join('\n'));
 	}
 
 	return parsed.data;
